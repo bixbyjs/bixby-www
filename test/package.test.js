24e6dcae5e58afc99a32dea98f14104d5f453e29
Jared Hanson <jaredhanson@gmail.com>
2015-08-20 17:13:06 -0700

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('nodex-www', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
